c8ab85a8ec7aa720cc8b68fc23d74232e0fa84d2
julio_filizzola <juliofilizzola@hotmail.com>
2023-04-13 06:58:33 -0300

    feat: create config jsonContainerFactory

--- src/main/java/com/filizzola/jsonconsumer/config/JsonConsumerConfig.java
+++ src/main/java/com/filizzola/jsonconsumer/config/JsonConsumerConfig.java
@@ -6,8 +6,10 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.converter.JsonMessageConverter;
 
 import java.util.HashMap;
 
@@ -23,5 +25,15 @@ public class JsonConsumerConfig {
     config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
     config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
     return new DefaultKafkaConsumerFactory<>(config);
+  }
+
+  @Bean
+  public ConcurrentKafkaListenerContainerFactory jsonContainerFactory(
+    ConsumerFactory<String, Object> jsonConsumerFactory
+  ) {
+    var factory = new ConcurrentKafkaListenerContainerFactory<String, Object>();
+    factory.setConsumerFactory(jsonConsumerFactory);
+    factory.setRecordMessageConverter(new JsonMessageConverter());
+    return factory;
   }
 }
